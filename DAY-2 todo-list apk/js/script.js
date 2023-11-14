6500bf79772e8bc05306096aa68f872e08a5fcbc
Divine-love <divineloveshizzy08@gmail.com>
2023-11-14 04:30:52 +0100

    i tire

--- DAY-2 todo-list apk/js/script.js
+++ DAY-2 todo-list apk/js/script.js
@@ -1,14 +1,1 @@
-function addTask() {
-    var taskList = document.getElementById("task-list");
-    var taskInput = document.getElementById("new-task");
-    var task = taskInput.value;
-    if (task.trim() !== "") {
-        var li = document.createElement("li");
-        li.textContent = task;
-        li.classList.add("task-item");
-        taskList.appendChild(li);
-        taskInput.value = "";
-    } else {
-        alert("Please enter a task!");
-    }
-}
+
