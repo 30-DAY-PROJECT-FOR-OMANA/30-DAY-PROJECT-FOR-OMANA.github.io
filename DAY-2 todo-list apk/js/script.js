93097d2eb1e06e3418d4cd6fedb798efb7324164
Divine-love <divineloveshizzy08@gmail.com>
2023-11-14 03:48:52 +0100

    new things

--- DAY-2 todo-list apk/js/script.js
+++ DAY-2 todo-list apk/js/script.js
@@ -1,10 +1,14 @@
-document.getElementById("addButton").addEventListener("click", function() {
-    var taskContainer = document.getElementById("list-box");
-
-    // Create a new textarea element
-    var newTask = document.createElement("textarea");
-    newTask.placeholder = "Enter your task...";
-
-    // Append the new textarea to the taskContainer
-    taskContainer.appendChild(newTask);
-});
+function addTask() {
+    var taskList = document.getElementById("task-list");
+    var taskInput = document.getElementById("new-task");
+    var task = taskInput.value;
+    if (task.trim() !== "") {
+        var li = document.createElement("li");
+        li.textContent = task;
+        li.classList.add("task-item");
+        taskList.appendChild(li);
+        taskInput.value = "";
+    } else {
+        alert("Please enter a task!");
+    }
+}
